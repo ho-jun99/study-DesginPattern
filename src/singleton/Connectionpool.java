280553d1892b5772669d586ca626c39e567ab7e0
‍김호준(학부생-소프트웨어전공) <hjk6322@kookmin.ac.kr>
2022-09-16 00:41:04 +0900

    #create singleton

--- src/singleton/Connectionpool.java
+++ src/singleton/Connectionpool.java
@@ -1,4 +1,18 @@
 package singleton;
 
+
+/*
+
+ */
 public class Connectionpool {
+    private static Connectionpool connectionpool = new Connectionpool();
+    private Connectionpool(){}
+
+    public static Connectionpool getInstance() {
+        if ( connectionpool == null) {
+            connectionpool = new Connectionpool();
+        }
+        return connectionpool;
+    }
+
 }
---
+++ src/singleton/Test.java
@@ -0,0 +1,19 @@
+package singleton;
+
+public class Test {
+
+    public static void main(String args[]) {
+
+        // Connectionpool con = new Connectionpool() 인스턴스를 생성 할 수 없음.
+
+        Connectionpool con1 = Connectionpool.getInstance();
+        Connectionpool con2 = Connectionpool.getInstance();
+
+        System.out.println("con1 : " + con1);
+        System.out.println("con2 : " + con1);
+        System.out.println("con1 == con2 : " +  con1 == con2.toString());
+
+        
+
+    }
+}
